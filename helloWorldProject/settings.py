1391b08c2738b8646b5bdab317c62cf50984a70f
rikutokd <rikutokd@yahoo.co.jp>
2023-03-05 01:31:51 +0900

    secretKey 再生成

--- helloWorldProject/settings.py
+++ helloWorldProject/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#md=@p3*!+7zc6ba!4v)7ovpppys9(uw805-^vx$kx(96^@p1i'
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
